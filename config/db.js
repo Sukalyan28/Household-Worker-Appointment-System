2cf5e51da3608ec0fb351a565b453c5fc2b8cd46
debargha-2002 <ddeb36680@gmail.com>
2024-10-24 02:23:26 +0530

    Added dotenv

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
-
 require("dotenv").config();
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
