149b56e18ed44e3e0ee5612ee77fe606da67bf4d
Sukalyan Chakraborty <arg2002suku@gmail.com>
2024-11-23 23:00:46 +0530

    ServEase Named

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -83,7 +83,7 @@ const Layout = ({ children }) => {
         <div className="layout">
           <div className="sidebar">
             <div className="logo">
-              <h6>HomeEase</h6>
+              <h6>ServEase</h6>
               <hr />
             </div>
             <div className="menu">
