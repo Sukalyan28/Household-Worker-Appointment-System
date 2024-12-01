e3e3a9e05ace457779c2dd78c2ca3257b44bafc7
debargha-2002 <ddeb36680@gmail.com>
2024-12-01 21:28:45 +0530

    UI/UX changed

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -34,21 +34,21 @@ const Login = () => {
     }
   };
   return (
-    <div className="form-container ">
+    <div className="form-container bg-[#fff4c9]">
       <Form
         layout="vertical"
         onFinish={onfinishHandler}
-        className="register-form"
+        className=" border-2 border-black rounded-md px-3 pb-5 pt-3"
       >
-        <h3 className="text-center">Login</h3>
+        <h3 className="text-center font-semibold text-xl">Login</h3>
 
         <Form.Item label="Email" name="email">
-          <Input type="email" required />
+          <Input type="email" placeholder="Enter your Email" required />
         </Form.Item>
         <Form.Item label="Password" name="password">
-          <Input type="password" required />
+          <Input type="password" placeholder=" Enter your password" required />
         </Form.Item>
-        <Link to="/register" className="m-2">
+        <Link to="/register" className="m-2 text-blue-100">
           Not a user Register here
         </Link>
         <button className="btn btn-primary" type="submit">
